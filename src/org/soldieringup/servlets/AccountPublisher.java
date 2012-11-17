5553933897f426e9a36bbbb640acfe058ef56bd3
Jared Jennings <jared@jaredjennings.org>
2012-11-17 12:24:47 -0600

    Formating Update

--- src/org/soldieringup/servlets/AccountPublisher.java
+++ src/org/soldieringup/servlets/AccountPublisher.java
@@ -14,25 +14,28 @@ import org.soldieringup.Engine;
  * Servlet implementation class PublisherAccounts
  */
 @WebServlet({ "/PublisherAccounts", "/paccct" })
-public class AccountPublisher extends HttpServlet {
-	private static final long serialVersionUID = 1L;
-	private static final String _cmd = "cmd";
-	static final Logger log = Logger.getLogger (AccountPublisher.class.getName ());
-       
-    /**
-     * @see HttpServlet#HttpServlet()
-     */
-    public AccountPublisher() {
-        super();
-        // TODO Auto-generated constructor stub
-    }
+public class AccountPublisher extends HttpServlet
+{
+	private static final long	serialVersionUID	= 1L;
+	private static final String	_cmd			= "cmd";
+	static final Logger		log			= Logger.getLogger (AccountPublisher.class.getName ());
+
+	/**
+	 * @see HttpServlet#HttpServlet()
+	 */
+	public AccountPublisher ()
+	{
+		super ();
+		// TODO Auto-generated constructor stub
+	}
 
 	/**
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	protected void doGet (HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException
+	{
 		String cmd = request.getParameter (_cmd);
-		
+
 		if (cmd != null) {
 			switch (cmd) {
 				case "roster":
@@ -46,10 +49,11 @@ public class AccountPublisher extends HttpServlet {
 	/**
 	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
 	 */
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	protected void doPost (HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException
+	{
 		// TODO Auto-generated method stub
 	}
-	
+
 	private void retrieveAccounts (HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException
 	{
 		Engine engine = new Engine ();
