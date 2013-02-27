5169af0bc82c1308e22e0a0748fb208c15ce60d0
JakeLaCombe <jake.lacombe2@gmail.com>
2013-02-26 23:55:20 -0600

    Added database columns and validation method.
    Added in the enumerations and strings associated to those columns,
    and also added in a method that can be used to validate that a
    key and a value associated to that key are valid inputs for the
    user table in a database.

--- src/org/soldieringup/User.java
+++ src/org/soldieringup/User.java
@@ -1,7 +1,54 @@
 package org.soldieringup;
 
+import org.soldieringup.Business.BusinessDatabaseColumns;
+
+/**
+ * Class represents a user in the database 
+ * @author Jake
+ *
+ */
 public class User 
 {
+	public static enum UserDatabaseColumns
+	{
+		ID (0),
+		FIRST_NAME (1),
+		LAST_NAME (2),
+		EMAIL (3),
+		ADDRESS (4),
+		PRIMARY_NUMBER (5),
+		SECONDARY_NUMBER (6),
+		PASSWORD (7),
+		SALT (8),
+		ZIP (9);
+		
+		int mDatabaseTableIndex;
+		
+		private UserDatabaseColumns( int aDatabaseTableIndex )
+		{
+			this.mDatabaseTableIndex = aDatabaseTableIndex;
+		}
+		
+		public int getDatabaseColumnIndex()
+		{
+			return mDatabaseTableIndex;
+		}
+	}
+	
+	public static String UserDatabaseColumnsStrings[] =
+		{
+		"id",
+		"first_name",
+		"last_name",
+		"email",
+		"address",
+		"primary_number",
+		"secondary_number",
+		"password",
+		"salt",
+		"ZIP"
+		};
+	
 	private long id;
 	private String first_name;
 	private String last_name;
@@ -94,5 +141,31 @@ public class User
 	public void setZip( String zip )
 	{
 		this.zip = zip;
+	}
+	
+
+	/**
+	 * Makes that a given key and it's associated value are valid inputs
+	 * for the database
+	 * @param aKey Key to check
+	 * @param aValue Value associated to the key
+	 * @return True if the input is valid for the database, false otherwise
+	 */
+	public static boolean isValidDatabaseInput( String aKey, String aValue )
+	{
+		if( Utilities.isElementInArray( aKey, UserDatabaseColumnsStrings) )
+		{
+			if( aKey == UserDatabaseColumnsStrings[UserDatabaseColumns.ZIP.getDatabaseColumnIndex()] ||
+				aKey == UserDatabaseColumnsStrings[UserDatabaseColumns.PRIMARY_NUMBER.getDatabaseColumnIndex()] ||
+				aKey == UserDatabaseColumnsStrings[UserDatabaseColumns.SECONDARY_NUMBER.getDatabaseColumnIndex()])
+			{
+				return Utilities.stringIsNumeric( aValue );
+			}
+			else
+			{
+				return aValue != null;
+			}
+		}
+		return false;
 	}
 }
