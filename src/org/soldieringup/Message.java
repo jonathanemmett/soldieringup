c0ffaae1c029079f25493b4e989f132fb86f3ccc
Jared Jennings <jaredljennings@gmail.com>
2013-04-13 23:55:29 -0500

    Example of JSON with Messaging support.

---
+++ src/org/soldieringup/Message.java
@@ -0,0 +1,109 @@
+package org.soldieringup;
+
+import java.util.GregorianCalendar;
+
+import org.bson.types.ObjectId;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.DBRef;
+
+/**
+ * Handles the different messages that a Vet and Ent. might pass between each other.
+ * @author jjennings
+ *
+ */
+public class Message
+{
+	@Id
+	private ObjectId id;
+	private ObjectId parentId;
+	private String body;
+	@DBRef
+	private ObjectId authorId;
+	private String title;
+	private GregorianCalendar date;
+
+	/**
+	 * @return the id
+	 */
+	public ObjectId getId ()
+	{
+		return id;
+	}
+	/**
+	 * @param id the id to set
+	 */
+	public void setId (ObjectId id)
+	{
+		this.id = id;
+	}
+	/**
+	 * @return the parentId
+	 */
+	public ObjectId getParentId ()
+	{
+		return parentId;
+	}
+	/**
+	 * @param parentId the parentId to set
+	 */
+	public void setParentId (ObjectId parentId)
+	{
+		this.parentId = parentId;
+	}
+	/**
+	 * @return the body
+	 */
+	public String getBody ()
+	{
+		return body;
+	}
+	/**
+	 * @param body the body to set
+	 */
+	public void setBody (String body)
+	{
+		this.body = body;
+	}
+	/**
+	 * @return the authorId
+	 */
+	public ObjectId getAuthorId ()
+	{
+		return authorId;
+	}
+	/**
+	 * @param authorId the authorId to set
+	 */
+	public void setAuthorId (ObjectId authorId)
+	{
+		this.authorId = authorId;
+	}
+	/**
+	 * @return the title
+	 */
+	public String getTitle ()
+	{
+		return title;
+	}
+	/**
+	 * @param title the title to set
+	 */
+	public void setTitle (String title)
+	{
+		this.title = title;
+	}
+	/**
+	 * @return the date
+	 */
+	public GregorianCalendar getDate ()
+	{
+		return date;
+	}
+	/**
+	 * @param date the date to set
+	 */
+	public void setDate (GregorianCalendar date)
+	{
+		this.date = date;
+	}
+}
---
+++ src/org/soldieringup/controllers/JSONMessages.java
@@ -0,0 +1,59 @@
+package org.soldieringup.controllers;
+
+import java.util.GregorianCalendar;
+
+import org.bson.types.ObjectId;
+import org.soldieringup.Message;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import com.google.gson.Gson;
+
+@Controller
+@RequestMapping("/messages")
+public class JSONMessages {
+	// Example:
+	//http://localhost:8080/soldieringup/rest/messages
+
+	@RequestMapping(method = RequestMethod.GET)
+	public @ResponseBody String handleGetMessages(@RequestParam String email) {
+		System.out.println ("Received request for email:" + email);
+		Gson gson = new Gson();
+		return gson.toJson(getMessage (email));
+	}
+
+	@RequestMapping(params = {"msgID"}, method = RequestMethod.GET)
+	public @ResponseBody String handleGetMessage(@RequestParam String msgID) {
+		System.out.println ("Received request for msgID:" + msgID);
+		Gson gson = new Gson();
+		return gson.toJson(getMessage (msgID));
+	}
+
+	//http://localhost:8080/soldieringup/rest/messages/msg.do?msgID=<id>
+	@RequestMapping(method = RequestMethod.POST)
+	public @ResponseBody String handlePostMessages(@RequestParam String msgID) {
+		Gson gson = new Gson();
+		return gson.toJson(getMessage (msgID));
+	}
+
+	@RequestMapping(params = {"msgID"}, method = RequestMethod.POST)
+	public @ResponseBody String handlePostMessage(@RequestParam String msgID) {
+		System.out.println ("Received request for msgID:" + msgID);
+		Gson gson = new Gson();
+		return gson.toJson(getMessage (msgID));
+	}
+
+	private Message getMessage (String msgID)
+	{
+		Message msg = new Message ();
+		msg.setId (new ObjectId ());
+		msg.setAuthorId (new ObjectId ());
+		msg.setTitle ("Question");
+		msg.setBody ("Have you ever done this before");
+		msg.setDate (new GregorianCalendar ());
+		return msg;
+	}
+}
