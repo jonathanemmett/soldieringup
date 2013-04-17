f3402f8d1c47408ad670adfd30f98a9c358946e5
Jared Jennings <jaredljennings@gmail.com>
2013-04-17 01:28:47 -0500

    Security Sorta Works now for the application. Is not integrated with the database, but do have a mock user
    http://localhost:8080/soldieringup/rest/account
    userID=mkyong
    password=123456

--- src/org/soldieringup/controllers/JSONController.java
+++ src/org/soldieringup/controllers/JSONController.java
@@ -4,6 +4,8 @@ import org.soldieringup.Business;
 import org.soldieringup.DIVISION;
 import org.soldieringup.User;
 import org.soldieringup.Veteran;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -13,15 +15,23 @@ import org.springframework.web.bind.annotation.ResponseBody;
 import com.google.gson.Gson;
 
 @Controller
-@RequestMapping("/accounts")
+@RequestMapping("/rest")
 public class JSONController {
 	// Example:
 	//http://localhost:8080/soldieringup/rest/account/jared
 
-	@RequestMapping(method = RequestMethod.GET)
+	@RequestMapping(value="accounts", method = RequestMethod.GET)
 	public @ResponseBody String getShopInJSON(@RequestParam String email) {
 		Gson gson = new Gson();
 		return gson.toJson(getUser (email));
+	}
+
+	@RequestMapping(value="account", method = RequestMethod.GET)
+	public @ResponseBody String getAccount() {
+		Gson gson = new Gson();
+		Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+		String name = auth.getName(); //get logged in username
+		return gson.toJson(getUser (name));
 	}
 
 	private User getUser (String email)
